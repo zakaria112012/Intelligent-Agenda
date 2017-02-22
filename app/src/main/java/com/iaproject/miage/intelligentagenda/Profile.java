704561f866f9dff305858738169f739c1b804529
zakaria112012 <m.zak-3@hotmail.com>
2017-02-22 19:14:16 +0100

    Signin Activity

--- app/src/main/java/com/iaproject/miage/intelligentagenda/Profile.java
+++ app/src/main/java/com/iaproject/miage/intelligentagenda/Profile.java
@@ -1,8 +1,9 @@
 package com.iaproject.miage.intelligentagenda;
 
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,7 +44,7 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
-
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
 
             }
         });
