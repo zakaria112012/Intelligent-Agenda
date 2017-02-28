5787f407b81ab8070459c0653c843956be03e14a
Oussema <o.oulhaci@gmail.com>
2017-02-28 14:38:31 +0100

    Update model and algo to add several events

---
+++ app/src/main/java/com/iaproject/miage/intelligentagenda/exception/AddEventException.java
@@ -0,0 +1,11 @@
+package com.iaproject.miage.intelligentagenda.exception;
+
+/**
+ * Created by OOussema on 28/02/2017.
+ */
+
+public class AddEventException extends Exception {
+
+	public AddEventException() {
+	}
+}
